6414a307f01e94a4e2260d6c67f3c1007e7d3909
Aveta Vos <avetavos@hotmail.com>
2018-11-29 14:33:30 +0700

    Updates

---
+++ precache-manifest.4281623b7f82ded431de535b4b494eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3c48b9cdbff86df9e4",
+    "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
+  },
+  {
+    "revision": "e07264fc720371516ffe",
+    "url": "/react-contact-manager/static/js/main.e07264fc.chunk.js"
+  },
+  {
+    "revision": "559311c98d9154ecdc66",
+    "url": "/react-contact-manager/static/js/1.559311c9.chunk.js"
+  },
+  {
+    "revision": "e07264fc720371516ffe",
+    "url": "/react-contact-manager/static/css/main.ce0e4b88.chunk.css"
+  },
+  {
+    "revision": "559311c98d9154ecdc66",
+    "url": "/react-contact-manager/static/css/1.2db0abc5.chunk.css"
+  },
+  {
+    "revision": "af5cac70202bf8a047a7eb431493fb47",
+    "url": "/react-contact-manager/index.html"
+  }
+];
